50561727a24555719783ab786c5137a961db8682
Ivan <ivan-dp@bk.ru>
2019-09-08 22:12:33 +0300

    Третий коммит

---
+++ Project1/js/js.js
@@ -0,0 +1,57 @@
+// let x = 5;
+// alert (x++);                                // ответ 5
+
+// let y = 1; 
+// let x = y = 2; 
+// alert(x);                                   // ответ 2
+
+// let i = [ ] + 1 + 2;                        // ответ 12
+// alert (i);
+
+// let i = [ ] + false - null + true;
+// alert (i);                                  // ответ NaN
+
+
+// alert( "1"[0] );                            // ответ 1
+
+
+// let i = 2 && 1 && null && 0 && undefined;
+// console.log(i);                             // ответ null
+
+
+
+// let a = 7;
+// let b = 8;
+// let i = !!(a && b);
+// let c = (a && b);
+// if (i = c) {
+// 	console.log('TRUE')
+// } else {
+// 	console.log('FALSE')
+// }                                           // ответ TRUE
+
+
+// alert( null || 2 && 3 || 4 );               // ответ 3
+
+
+// let a = [1, 2, 3];
+// let b = [1, 2, 3];
+
+// if (a == b) {
+// 	console.log('true')
+// } else {
+// 	console.log('false')
+// }                                           // ответ false
+
+
+
+// alert( +"Infinity" );                       // ответ Infinity
+
+
+// if ("ёжик" > "яблоко") {
+// 	console.log('true')
+// } else {
+// 	console.log('false')
+// }                                           // ответ true
+
+
--- Project1/js/script.js
+++ Project1/js/script.js
@@ -9,7 +9,7 @@ alert (age);*/
 /*var money;
 var shop;*/
 money = prompt ("Каким бюджетом вы располагаете?");
-shop = prompt ("В каком магазине вы затариваетесь?");
+name = prompt ("В каком магазине вы затариваетесь?");
 var shopGoods = ['blackDick', 'horseDick', 'vagina', 'littleAss'];
 /*var employers = ['Rachel', 'Shona', 'Mircella', 'Angelina', 'Pamella'];*/
 
@@ -49,11 +49,11 @@ employers = {
 	};
 
 mainList = {
-	money,
-	shop,
-	shopGoods,
-	employers,
-	open,
+	budget: money,
+	shopName: name,
+	shopGoods: [],
+	employers: {},
+	open: false
 }
 
 /*console.log(mainList);*/
@@ -63,13 +63,72 @@ console.log(money);
 console.log(shopGoods);
 console.log(mainList);
 
-var shopGoods = [
-	prompt ("Какой тип товаров будем продавать?"),
-	prompt ("Какой тип товаров будем продавать?"), 
-	prompt ("Какой тип товаров будем продавать?"),
-];
+for (let i=0; i < 5; i++) {
+
+	let a = prompt("Какой тип товаров будем продавать?");
+	
+	if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50) {
+		console.log('Всё верно');
+		mainList.shopGoods[i] = a;
+	}
+}
 
 
-console.log(shopGoods);
 
-alert (money / 30);
+
+// var shopGoods = [
+// 	prompt ("Какой тип товаров будем продавать?"),
+// 	prompt ("Какой тип товаров будем продавать?"), 
+// 	prompt ("Какой тип товаров будем продавать?"),
+// ];
+
+alert (mainList.budget / 30);
+
+console.log (mainList);
+
+// let num = 50;
+
+// if (num < 49) {
+// 	console.log('Неверно')
+// } else if (num > 100) {
+// 	console.log('Неверно')
+// } else {
+// 	console.log('Верно')
+// }
+
+// //(num == 50)? console.log('Верно') : console.log('Неверно');
+
+// switch (num) {
+// 	case 49:
+// 		console.log('Мало');
+// 		break;
+// 	case 100:
+// 		console.log('Много');
+// 		break;
+// 	case 80:
+// 		console.log('Всё ещё много');
+// 		break;
+// 	case 50:
+// 		console.log('В точку');
+// 		break;
+// 	default:
+// 		console.log('Не в этот раз');
+// 		break;
+// }
+
+// // while (num < 55) {
+// // 	console.log(num);
+// // 	num++;
+// // }
+
+
+// // do {
+// // 	console.log(num);
+// // 	num++;
+// // }
+// // while (num < 55); 
+
+
+// for (let i = 0; i < 8; i++) {
+// 	console.log(i);
+// }
